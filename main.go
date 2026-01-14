b75a37f6de203a04bf7cda388b3acf18534d2d91
vankos <14087195+vankos@users.noreply.github.com>
2026-01-14 11:01:07 +0700

    add skeleton

--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/gin-gonic/gin"
+)
+
+var urlsMap map[string]string = make(map[string]string)
+
 func main() {
+	router := gin.Default()
+	router.POST("/create", createShortLink)
+	router.GET("/get", expendShortLink)
+	router.Run("localhost:8080")
+}
+
+func createShortLink(ginContext *gin.Context) {
+	urlToShorten := ginContext.GetString("url")
+	isUrl := validateUrl(urlToShorten)
+	if isUrl {
+		shortUrl := generateShortUrl(urlToShorten)
+	}
+	ginContext.String(http.StatusOK)
+}
+
+func generateShortUrl(urlToShorten string) string {
+	"localhost:8080/" + ""
+}
+
+func validateUrl(urlToShorten string) bool {
+	_, err := url.Parse(urlToShorten)
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+func expendShortLink(ginContext *gin.Context) {
 
 }
