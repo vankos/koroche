d6254700690909972109d7d080df7c68ea2ccbed
vankos <14087195+vankos@users.noreply.github.com>
2026-01-26 12:15:02 +0700

    simplify for

--- shortUrlGenerator/shortUrlGenerator.go
+++ shortUrlGenerator/shortUrlGenerator.go
@@ -10,7 +10,7 @@ const base62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 func GenerateShortUrl(host string, urlSize int) string {
 
 	var randomPart string = ""
-	for i := 0; i < urlSize; i++ {
+	for range urlSize {
 		randIndex, _ := rand.Int(rand.Reader, big.NewInt(int64(len(base62))))
 		randBase62Char := base62[randIndex.Int64()]
 		randomPart += string(randBase62Char)
