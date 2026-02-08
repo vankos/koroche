b412cd382a03e338cd5e746dabae86eae1ccf33b
vankos <14087195+vankos@users.noreply.github.com>
2026-02-08 16:28:56 +0700

    fix interface

--- urlStorage/urlStorage.go
+++ urlStorage/urlStorage.go
@@ -3,7 +3,7 @@ package urlStorage
 // UrlStorage is an interface for URL storage implementations
 type UrlStorage interface {
 	// Saves the mapping between the original URL and the shortened URL
-	Store(string string) error
+	Store(string, string) error
 	// Retrieves the original URL based on the shortened URL
 	Get(string) (string, error)
 	// Increments the click count for a given shortened URL
