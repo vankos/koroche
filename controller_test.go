3f2085db1bbc7e85a31423d0ea36c12d5686ed6a
vankos <14087195+vankos@users.noreply.github.com>
2026-04-05 21:15:55 +0300

    add controller tests

---
+++ controller_test.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"koroche/urlStorage"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetUpRouter() *gin.Engine {
+	urlStorage := urlStorage.NewInMemoryUrlStorage()
+	statsChannel := make(chan string, 100)
+	shortUrlHostName := "http://localhost:8080"
+	controller := NewController(&urlStorage, statsChannel, shortUrlHostName)
+	router := SetupRouter(controller)
+	return router
+}
+
+func TestСreate(t *testing.T) {
+	router := SetUpRouter()
+	testData := []struct {
+		requestUrl   string
+		expectedCode int
+		expectString bool
+	}{
+		{"https://www.google.com", http.StatusOK, true},
+		{"invalid-url", http.StatusUnprocessableEntity, false},
+	}
+
+	for _, data := range testData {
+		t.Run(data.requestUrl, func(t *testing.T) {
+			request, _ := http.NewRequest("POST", "/create?url="+data.requestUrl, nil)
+			recorder := httptest.NewRecorder()
+			router.ServeHTTP(recorder, request)
+			assert.Equal(t, data.expectedCode, recorder.Code)
+			assert.Equal(t, recorder.Body.String() != "", data.expectString)
+		})
+
+	}
+}
+
+func TestGet(t *testing.T) {
+	router := SetUpRouter()
+	originalUrl := "https://www.google.com"
+	request, _ := http.NewRequest("POST", "/create?url="+originalUrl, nil)
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+	shortenUrl := recorder.Body.String()
+	testData := []struct {
+		shortUrl     string
+		expectedCode int
+		expectString string
+	}{
+		{shortenUrl, http.StatusOK, originalUrl},
+		{"invalid-short-url", http.StatusUnprocessableEntity, ""},
+		{originalUrl + "/invalid-short-url", http.StatusNotFound, ""},
+	}
+
+	for _, data := range testData {
+		t.Run(data.shortUrl, func(t *testing.T) {
+			request, _ := http.NewRequest("GET", "/get?url="+data.shortUrl, nil)
+			recorder := httptest.NewRecorder()
+			router.ServeHTTP(recorder, request)
+			assert.Equal(t, data.expectedCode, recorder.Code)
+			assert.Equal(t, recorder.Body.String(), data.expectString)
+		})
+
+	}
+}
+
+func TestStats(t *testing.T) {
+	router := SetUpRouter()
+	originalUrl := "https://www.google.com"
+	request, _ := http.NewRequest("POST", "/create?url="+originalUrl, nil)
+	recorder := httptest.NewRecorder()
+	router.ServeHTTP(recorder, request)
+	shortenUrl := recorder.Body.String()
+	testData := []struct {
+		shortUrl     string
+		expectedCode int
+		expectString string
+	}{
+		{shortenUrl, http.StatusOK, originalUrl},
+		{shortenUrl, http.StatusOK, originalUrl},
+		{"invalid-short-url", http.StatusUnprocessableEntity, ""},
+		{originalUrl + "/invalid-short-url", http.StatusNotFound, ""},
+	}
+
+	for index, data := range testData {
+		testName := fmt.Sprintf("%s_%d", data.shortUrl, index)
+		t.Run(testName, func(t *testing.T) {
+			statsRequest, _ := http.NewRequest("GET", "/stats?url="+data.shortUrl, nil)
+			statsRecorder := httptest.NewRecorder()
+			router.ServeHTTP(statsRecorder, statsRequest)
+			if data.expectedCode == http.StatusOK {
+				jsonData := statsRecorder.Body.Bytes()
+				var stats urlStorage.LinkStats
+				err := json.Unmarshal(jsonData, &stats)
+				assert.NoError(t, err)
+				assert.Equal(t, index, stats.ClickCount)
+				assert.Equal(t, data.expectString, stats.OriginalUrl)
+				assert.Equal(t, data.shortUrl, stats.ShortUrl)
+			}
+
+			IncrementStats(data, router)
+			assert.Equal(t, data.expectedCode, statsRecorder.Code)
+		})
+
+	}
+}
+
+func IncrementStats(data struct {
+	shortUrl     string
+	expectedCode int
+	expectString string
+}, router *gin.Engine) {
+	getRequest, _ := http.NewRequest("GET", "/get?url="+data.shortUrl, nil)
+	getRecorder := httptest.NewRecorder()
+	router.ServeHTTP(getRecorder, getRequest)
+}
