be29f8862c8464c12c57bc9c35202ddf66d33ae0
vankos <14087195+vankos@users.noreply.github.com>
2026-07-05 21:46:34 +0300

    add struct tags to LinkStats

--- urlStorage/linkStats.go
+++ urlStorage/linkStats.go
@@ -5,13 +5,13 @@ import "time"
 // LinkStats represents the statistics of a shortened URL
 type LinkStats struct {
 	// ShortUrl is the shortened URL
-	ShortUrl string
+	ShortUrl string `json:"short_url"`
 	// OriginalUrl is the original URL that was shortened
-	OriginalUrl string
+	OriginalUrl string `json:"original_url"`
 	// CreatedAt is the time when the short URL was created
-	CreatedAt time.Time
+	CreatedAt time.Time `json:"created_at"`
 	// LastAccessedAt is the time when the short URL was last accessed
-	LastAccessedAt *time.Time
+	LastAccessedAt *time.Time `json:"last_accessed_at,omitempty"`
 	// ClickCount is the number of times the short URL has been accessed
-	ClickCount int
+	ClickCount int `json:"click_count"`
 }
